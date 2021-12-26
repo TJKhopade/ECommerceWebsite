0cdff192640dc8a0e93f4fb251872786af05804d
Tejas <Tejas@DESKTOP-4B1Q52I>
2021-12-27 02:13:02 +0530

    Add project files.

---
+++ ECommerceWebsite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using ECommerceWebsite.Dal;
+using ECommerceWebsite.Models;
+using ECommerceWebsite.Models.Home;
+using ECommerceWebsite.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ECommerceWebsite.Controllers
+{
+	public class HomeController : Controller
+	{
+		MaharajaEDBEntities ctx = new MaharajaEDBEntities();
+
+		public ActionResult Index(string search, int? page)
+		{
+			HomeIndexViewModel model = new HomeIndexViewModel();
+			return View(model.CreateModel(search, 4, page));
+		}
+
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ ECommerceWebsite/Models/Home/Item.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceWebsite.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceWebsite.Models.Home
+{
+    public class Item
+    {
+        public Tbl_Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
